75eeadf4d90821f35e11494f6e5aa4657e9920c8
Jessica Ames <jessicaames@Jessicas-MacBook-Pro-2.local>
2020-08-07 16:30:04 -0600

    cards

--- js/main.js
+++ js/main.js
@@ -41,3 +41,80 @@ people
         li.textContent = person.name + ", " + person.mass;
         peopleList.appendChild(li);
     });
+
+
+//////////////////////////////////////////////////////////////
+/////////// BASEBALL CARDS ASSIGNMENT
+//////////////////////////////////////////////////////////////
+
+const parkList = document.querySelector("#parks");
+
+const appendCard = park => {
+    const li = document.createElement("li");
+    li.classList.add("parkscard");
+    li.addEventListener("click", () => {
+        li.classList.toggle("is-flipped")
+    });
+
+    // Create and append front card
+    const front = document.createElement("div");
+    front.classList.add("parkscard-front");
+        front.classList.add("parkscard-face");
+    li.appendChild(front);
+
+    const name = document.createElement("h3");
+    name.textContent = park.name;
+    front.appendChild(name);
+
+    // Create and append back card
+    const back = document.createElement("div");
+    back.classList.add("parkscard-back");
+        back.classList.add("parkscard-face");
+
+    li.appendChild(back);
+
+    const designation = document.createElement("div");
+    designation.textContent = "designation: " + park.designation;
+    back.appendChild(designation);
+
+    const states = document.createElement("div");
+    states.textContent = "states: " + park.states;
+    back.appendChild(states);
+    
+    const topActivity = document.createElement("div");
+    topActivity.textContent = "top activity: " + park.topActivity;
+    back.appendChild(topActivity);
+
+    parkList.appendChild(li);
+    
+    return;
+}
+
+const url = "https://developer.nps.gov/api/v1/parks?limit=30&api_key=miCYMXRQUXtycQSgTSa1uRK48U3pBo7RUoxAwhLk";
+fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+    console.log(data);
+        document.querySelector("#parks-warning").style.display = 'none';
+
+        data.data
+            .map(park => {
+                if(park.activities.length > 0) {
+                    park.topActivity = park.activities[0].name;
+                }
+                return park;
+            })
+            .sort((a, b) => (a.name > b.name) ? 1 : -1)
+            .forEach(park => {
+                appendCard(park);
+            });
+})
+
+document.getElementById("new-park-button").addEventListener("mousedown", event => {
+    appendCard({
+        name: "my fake park",
+        topActivity:"Fishing",
+        states: "Utah",
+        designation: "Park"
+    });
+})
